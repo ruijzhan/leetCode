15e0712c3e2198dc81be23b1a664a680c573d1f3
Ruijian Zhang <qiudog825@gmail.com>
2021-10-01 23:56:08 +0800

    update 2

--- problems/linked_list/E2_AddTwoNumbers.go
+++ problems/linked_list/E2_AddTwoNumbers.go
@@ -48,3 +48,29 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	return head.Next
 }
+
+func addTwoNumbers2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	l1.Val += l2.Val
+	if l1.Val >= 10 {
+		l1.Val -= 10
+		if l1.Next == nil {
+			l1.Next = &ListNode{Val: 1}
+		} else {
+			l1.Next.Val++
+			if l2.Next == nil {
+				l2.Next = new(ListNode)
+			}
+		}
+	}
+
+	l1.Next = addTwoNumbers2(l1.Next, l2.Next)
+
+	return l1
+}
--- problems/linked_list/E2_AddTwoNumbers_test.go
+++ problems/linked_list/E2_AddTwoNumbers_test.go
@@ -31,16 +31,71 @@ func Test_addTwoNumbers(t *testing.T) {
 		},
 		{
 			args: args{
-				l1: newLinkedList(0),
-				l2: newLinkedList(0),
+				l1: nil,
+				l2: nil,
 			},
-			want: newLinkedList(0),
+			want: nil,
+		},
+		{
+			args: args{
+				l1: newLinkedList(2, 4, 3),
+				l2: nil,
+			},
+			want: newLinkedList(2, 4, 3),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := addTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("addTwoNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_addTwoNumbers2(t *testing.T) {
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			args: args{
+				l1: newLinkedList(2, 4, 3),
+				l2: newLinkedList(5, 6, 4),
+			},
+			want: newLinkedList(7, 0, 8),
+		},
+		{
+			args: args{
+				l1: newLinkedList(9, 9, 9, 9, 9, 9, 9),
+				l2: newLinkedList(9, 9, 9, 9),
+			},
+			want: newLinkedList(8, 9, 9, 9, 0, 0, 0, 1),
+		},
+		{
+			args: args{
+				l1: nil,
+				l2: nil,
+			},
+			want: nil,
+		},
+		{
+			args: args{
+				l1: newLinkedList(2, 4, 3),
+				l2: nil,
+			},
+			want: newLinkedList(2, 4, 3),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addTwoNumbers2(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("addTwoNumbers2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
