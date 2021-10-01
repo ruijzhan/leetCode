b1b0bbaf552afa632299fa430b81344d2d5f467c
Ruijian Zhang <qiudog825@gmail.com>
2021-10-01 21:49:37 +0800

    update 83 case

--- problems/linked_list/E83_RemoveDuplicatesFromSortedList_test.go
+++ problems/linked_list/E83_RemoveDuplicatesFromSortedList_test.go
@@ -49,3 +49,48 @@ func Test_deleteDuplicates(t *testing.T) {
 		})
 	}
 }
+
+func Test_deleteDuplicates2(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			args: args{head: nil},
+			want: nil,
+		},
+		{
+			args: args{head: &ListNode{0, nil}},
+			want: &ListNode{0, nil},
+		},
+		{
+			args: args{
+				head: newLinkedList(1, 1, 2, 2, 3, 3),
+			},
+			want: newLinkedList(1, 2, 3),
+		},
+		{
+			args: args{
+				head: newLinkedList(1, 1, 2),
+			},
+			want: newLinkedList(1, 2),
+		},
+		{
+			args: args{
+				head: newLinkedList(1, 1, 2, 2, 2, 2, 3, 3, 4, 4),
+			},
+			want: newLinkedList(1, 2, 3, 4),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteDuplicates2(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deleteDuplicates2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
