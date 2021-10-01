86376c6380d1c4fba18e03b3cc1bae704831bac1
Ruijian Zhang <qiudog825@gmail.com>
2021-10-01 21:22:47 +0800

    update 206

--- problems/linked_list/E206_ReverseLinkedList.go
+++ problems/linked_list/E206_ReverseLinkedList.go
@@ -7,14 +7,29 @@ package linkedlist
 func reverseList(head *ListNode) *ListNode {
 	var prev *ListNode
 
-	for head != nil {
-		next := head.Next
-		head.Next = prev
-		prev = head
-		head = next
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
 	}
 
 	return prev
 }
 
-//TODO: 用递归方法解题
+// 递归方法:
+// f(head) = f(head.Next) + 反转操作
+// 反转操作:  1,head 的下一个节点的 next 指向 head
+//           2,head 的 next 指向 nil
+func reverseList2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	newHead := reverseList2(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+
+	return newHead
+}
--- problems/linked_list/E206_ReverseLinkedList_test.go
+++ problems/linked_list/E206_ReverseLinkedList_test.go
@@ -53,3 +53,52 @@ func Test_reverseList(t *testing.T) {
 		})
 	}
 }
+
+func Test_reverseList2(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			args: args{
+				head: newLinkedList(),
+			},
+			want: nil,
+		},
+		{
+			args: args{
+				head: newLinkedList(0),
+			},
+			want: newLinkedList(0),
+		},
+		{
+			args: args{
+				head: newLinkedList(0, 1),
+			},
+			want: newLinkedList(1, 0),
+		},
+		{
+			args: args{
+				head: newLinkedList(0, 1, 2),
+			},
+			want: newLinkedList(2, 1, 0),
+		},
+		{
+			args: args{
+				head: newLinkedList(0, 1, 2, 3),
+			},
+			want: newLinkedList(3, 2, 1, 0),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseList2(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
