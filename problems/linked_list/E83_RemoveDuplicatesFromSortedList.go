1b6d5fb980f483e741c25bf8e9fc1211f49880e9
Ruijian Zhang <qiudog825@gmail.com>
2021-10-02 09:20:54 +0800

    update 83

--- problems/linked_list/E83_RemoveDuplicatesFromSortedList.go
+++ problems/linked_list/E83_RemoveDuplicatesFromSortedList.go
@@ -36,3 +36,18 @@ func deleteDuplicates2(head *ListNode) *ListNode {
 	}
 	return head
 }
+
+//递归
+func deleteDuplicates3(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	head.Next = deleteDuplicates3(head.Next)
+
+	if head.Val == head.Next.Val {
+		return head.Next
+	}
+
+	return head
+}
