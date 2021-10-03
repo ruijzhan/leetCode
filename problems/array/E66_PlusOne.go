a61a22ed45f789961a82dd763dddabb629ba0159
Ruijian Zhang <qiudog825@gmail.com>
2021-10-03 23:50:13 +0800

    update 66

--- problems/array/E66_PlusOne.go
+++ problems/array/E66_PlusOne.go
@@ -3,19 +3,19 @@ package array
 func plusOne(digits []int) []int {
 	ln := len(digits)
 	digits[ln-1] += 1
-	if digits[ln-1] != 10 {
-		return digits
-	}
 
 	for i := ln - 1; i > 0; i-- {
-		if digits[i] == 10 {
-			digits[i] = 0
+		if digits[i] < 10 {
+			return digits
+		} else {
+			digits[i] -= 10
 			digits[i-1] += 1
 		}
 	}
 
-	if digits[0] == 10 {
-		digits = append([]int{1, 0}, digits[1:]...)
+	if digits[0] > 9 {
+		digits[0] -= 10
+		digits = append([]int{1}, digits...)
 	}
 
 	return digits
