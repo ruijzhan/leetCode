b5cf0c212c0db107575e5ee3a646bb34ae366e99
Ruijian Zhang <qiudog825@gmail.com>
2021-10-05 10:35:01 +0800

    update 2

--- problems/linked_list/E2_AddTwoNumbers.go
+++ problems/linked_list/E2_AddTwoNumbers.go
@@ -3,52 +3,38 @@ package linkedlist
 // 空间复杂度 O(1)
 // 时间复杂度 O(N)
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	head := &ListNode{}
-
+	head := new(ListNode)
 	curr := head
-
 	add := 0
-	for l1 != nil && l2 != nil {
-		l1.Val = l1.Val + l2.Val + add
-		if l1.Val >= 10 {
-			add = 1
-			l1.Val -= 10
-		} else {
-			add = 0
+
+	for l1 != nil || l2 != nil {
+		if l1 == nil {
+			l1 = new(ListNode)
+		} else if l2 == nil {
+			l2 = new(ListNode)
 		}
+
+		l1.Val = l1.Val + l2.Val + add
+		add = 0
+		if l1.Val > 9 {
+			add = 1
+			l1.Val %= 10
+		}
+
 		curr.Next = l1
 		curr = curr.Next
 		l1 = l1.Next
 		l2 = l2.Next
-
-	}
-	var rest *ListNode
-	if l1 != nil {
-		rest = l1
-	} else {
-		rest = l2
 	}
 
-	for rest != nil {
-		rest.Val = rest.Val + add
-		if rest.Val >= 10 {
-			add = 1
-			rest.Val -= 10
-		} else {
-			add = 0
-		}
-		curr.Next = rest
-		curr = curr.Next
-		rest = rest.Next
-	}
-
-	if add == 1 {
-		curr.Next = &ListNode{Val: 1}
+	if add > 0 {
+		curr.Next = &ListNode{Val: add}
 	}
 
 	return head.Next
 }
 
+// 递归
 func addTwoNumbers2(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l1 == nil {
 		return l2
--- problems/linked_list/E2_AddTwoNumbers_test.go
+++ problems/linked_list/E2_AddTwoNumbers_test.go
@@ -3,9 +3,12 @@ package linkedlist
 import (
 	"reflect"
 	"testing"
+
+	"github.com/ruijzhan/leetCode/pkg/utils"
 )
 
 func Test_addTwoNumbers(t *testing.T) {
+	fs := []func(*ListNode, *ListNode) *ListNode{addTwoNumbers, addTwoNumbers2}
 	type args struct {
 		l1 *ListNode
 		l2 *ListNode
@@ -45,58 +48,13 @@ func Test_addTwoNumbers(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := addTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("addTwoNumbers() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_addTwoNumbers2(t *testing.T) {
-	type args struct {
-		l1 *ListNode
-		l2 *ListNode
-	}
-	tests := []struct {
-		name string
-		args args
-		want *ListNode
-	}{
-		{
-			args: args{
-				l1: newLinkedList(2, 4, 3),
-				l2: newLinkedList(5, 6, 4),
-			},
-			want: newLinkedList(7, 0, 8),
-		},
-		{
-			args: args{
-				l1: newLinkedList(9, 9, 9, 9, 9, 9, 9),
-				l2: newLinkedList(9, 9, 9, 9),
-			},
-			want: newLinkedList(8, 9, 9, 9, 0, 0, 0, 1),
-		},
-		{
-			args: args{
-				l1: nil,
-				l2: nil,
-			},
-			want: nil,
-		},
-		{
-			args: args{
-				l1: newLinkedList(2, 4, 3),
-				l2: nil,
-			},
-			want: newLinkedList(2, 4, 3),
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := addTwoNumbers2(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("addTwoNumbers2() = %v, want %v", got, tt.want)
-			}
-		})
+		for _, f := range fs {
+			t.Run(tt.name, func(t *testing.T) {
+				if got := f(tt.args.l1.DeepCopy(), tt.args.l2.DeepCopy()); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("%v(%v, %v) = %v, want %v",
+						utils.GetFunctionName(f), tt.args.l1, tt.args.l2, got, tt.want)
+				}
+			})
+		}
 	}
 }
