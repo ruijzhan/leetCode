23eeec63cb63786609ab94485656a4f058b8642e
Ruijian Zhang <qiudog825@gmail.com>
2021-10-05 11:34:23 +0800

    update 21

--- problems/linked_list/E21_MergeTwoLists.go
+++ problems/linked_list/E21_MergeTwoLists.go
@@ -1,11 +1,17 @@
 package linkedlist
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	var head *ListNode = &ListNode{}
-
+	head := new(ListNode)
 	curr := head
-	for l1 != nil && l2 != nil {
-		if l1.Val <= l2.Val {
+
+	for l1 != nil || l2 != nil {
+		if l1 == nil {
+			curr.Next = l2
+			l2 = l2.Next
+		} else if l2 == nil {
+			curr.Next = l1
+			l1 = l1.Next
+		} else if l1.Val <= l2.Val {
 			curr.Next = l1
 			l1 = l1.Next
 		} else {
@@ -13,12 +19,6 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 			l2 = l2.Next
 		}
 		curr = curr.Next
-	}
-
-	if l1 != nil {
-		curr.Next = l1
-	} else {
-		curr.Next = l2
 	}
 
 	return head.Next
--- problems/linked_list/E21_MergeTwoLists_test.go
+++ problems/linked_list/E21_MergeTwoLists_test.go
@@ -3,9 +3,12 @@ package linkedlist
 import (
 	"reflect"
 	"testing"
+
+	"github.com/ruijzhan/leetCode/pkg/utils"
 )
 
 func Test_mergeTwoLists(t *testing.T) {
+	fs := []func(*ListNode, *ListNode) *ListNode{mergeTwoLists, mergeTwoLists2}
 	type args struct {
 		l1 *ListNode
 		l2 *ListNode
@@ -45,58 +48,13 @@ func Test_mergeTwoLists(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := mergeTwoLists(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("mergeTwoLists() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_mergeTwoLists2(t *testing.T) {
-	type args struct {
-		l1 *ListNode
-		l2 *ListNode
-	}
-	tests := []struct {
-		name string
-		args args
-		want *ListNode
-	}{
-		{
-			args: args{
-				l1: nil,
-				l2: nil,
-			},
-			want: nil,
-		},
-		{
-			args: args{
-				l1: newLinkedList(1, 3, 5, 7, 9),
-				l2: nil,
-			},
-			want: newLinkedList(1, 3, 5, 7, 9),
-		},
-		{
-			args: args{
-				l1: newLinkedList(1, 3, 5, 7, 9),
-				l2: newLinkedList(2, 4, 6, 8, 10),
-			},
-			want: newLinkedList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
-		},
-		{
-			args: args{
-				l1: newLinkedList(1, 1, 2, 3, 3),
-				l2: newLinkedList(1, 1, 2, 2, 2),
-			},
-			want: newLinkedList(1, 1, 1, 1, 2, 2, 2, 2, 3, 3),
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := mergeTwoLists2(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("mergeTwoLists2() = %v, want %v", got, tt.want)
-			}
-		})
+		for _, f := range fs {
+			t.Run(tt.name, func(t *testing.T) {
+				if got := f(tt.args.l1.DeepCopy(), tt.args.l2.DeepCopy()); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("%v(%v, %v) = %v, want %v",
+						utils.GetFunctionName(f), tt.args.l1, tt.args.l2, got, tt.want)
+				}
+			})
+		}
 	}
 }
