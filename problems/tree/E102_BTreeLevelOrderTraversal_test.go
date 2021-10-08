bcd055948dfec328925cfabaa8570f8b98b79bcb
Ruijian Zhang <qiudog825@gmail.com>
2021-10-08 23:38:51 +0800

    102

---
+++ problems/tree/E102_BTreeLevelOrderTraversal_test.go
@@ -0,0 +1,31 @@
+package tree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_levelOrder(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			args: args{
+				root: New([]int{3, 9, 20, null, null, 15, 7}),
+			},
+			want: [][]int{{3}, {9, 20}, {15, 7}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := levelOrder(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("levelOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
