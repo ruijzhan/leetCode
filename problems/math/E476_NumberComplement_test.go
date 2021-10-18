2bc6c9b152170b881c95955d60ba4f8519a3b8d7
Ruijian Zhang <qiudog825@gmail.com>
2021-10-18 22:49:02 +0800

    476

---
+++ problems/math/E476_NumberComplement_test.go
@@ -0,0 +1,34 @@
+package math
+
+import "testing"
+
+func Test_findComplement(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			args: args{
+				num: 5,
+			},
+			want: 2,
+		},
+		{
+			args: args{
+				num: 1,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findComplement(tt.args.num); got != tt.want {
+				t.Errorf("findComplement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
