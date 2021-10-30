fffe03837b065ffd2f880d9cdd87993db3bcf0d6
Ruijian Zhang <qiudog825@gmail.com>
2021-10-30 10:30:15 +0800

    update 136

--- problems/array/E136_SingleNumber.go
+++ problems/array/E136_SingleNumber.go
@@ -45,15 +45,9 @@ func singleNumber3(nums []int) int {
 }
 
 // 位运算
-// 任何数和 0 做异或运算，结果仍然是原来的数
-// 任何数和其自身做异或运算，结果是 0
-// 异或运算满足交换律和结合律
-// https://leetcode-cn.com/problems/single-number/solution/zhi-chu-xian-yi-ci-de-shu-zi-by-leetcode-solution/
-
-func singleNumber4(nums []int) int {
-	single := 0
+func singleNumber4(nums []int) (single int) {
 	for _, num := range nums {
 		single ^= num
 	}
-	return single
+	return
 }
